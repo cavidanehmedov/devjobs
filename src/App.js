0d4f4d19cb4db4b2a1b536030c15761ca9c051e2
cavidanehmedov <104515083+cavidanehmedov@users.noreply.github.com>
2022-08-08 18:43:19 +0400

    Yusifin el iwi

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import "./app.scss";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
-
   return (
     <div className={`app ${darkMode ? "dark" : "light"}`}>
       <Header darkMode={darkMode} setDarkMode={setDarkMode} />
+         
+ 
+      
     </div>
+    
   );
 }
 
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Switch } from "../Switch";
 import "./style.scss";
 
+
 export const Header = ({ darkMode, setDarkMode }) => {
   return (
     <header className="header">
@@ -13,7 +14,11 @@ export const Header = ({ darkMode, setDarkMode }) => {
         />
         <Switch isDark={darkMode} setIsDark={setDarkMode} />
       </section>
-      <section className="header__search"></section>
+      <section className="header__search">
+        <div className="filter__title"> <img  src="assets/desktop/icon-search.svg" alt="icon" />Searcing </div>
+        <div className="filter__location"><img src="assets/desktop/icon-location.svg" alt="" /> Filter Location</div>
+        <div className="filter__searching"><img src="assets/desktop/icon-check" alt="" /><b>Full Time Only</b> <button className="header__button">Search</button> </div>
+      </section>
     </header>
   );
 };
